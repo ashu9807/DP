35ef7cce52e8eeaa93f88128bb2a10dc11ed84a2
Ashutosh Gupta <guptaashutosh584@gmail.com>
2024-07-09 22:33:21 +0530

    soved using memoisation

--- paintingfans.cpp
+++ paintingfans.cpp
@@ -14,3 +14,17 @@ int solveUsingRecursion(int n, int k) {
   int ans  = (solveUsingRecursion(n-2,k) + solveUsingRecursion(n-1,k))*(k-1);
   return ans;
 }
+int solveUsingMem(int n, int k, vector<int>& dp)  {
+    if(n == 1) {
+    return k ;
+  }
+  if(n == 2) {
+    return (k + k*(k-1));
+  }
+
+  if(dp[n] != -1)
+    return dp[n];
+
+  dp[n]  = (solveUsingMem(n-2,k,dp) + solveUsingMem(n-1,k,dp))*(k-1);
+  return dp[n];
+}
